c652e26dad224a3003c0efbec2df93b3baf33443
Tom Gulliver <tgulliver97@gmail.com>
2020-08-15 00:56:38 +0100

    started function for finding users in a radius

---
+++ geocoding.py
@@ -0,0 +1,25 @@
+from geopy import distance
+
+# 51°30′26″N 0°7′39″W
+LONDON_LATITUDE = 51 + (30 / 60) + (26 / 60 / 60)
+# 0°7′39″W
+LONDON_LONGITUDE = -((7 / 60.0) - (39 / 60.0 / 60.0))
+LONDON_COORDS = (LONDON_LATITUDE, LONDON_LONGITUDE)
+
+def users_in_radius(users, radius):
+
+    valid_users = dict()
+
+    for user in users:
+        user_lat = user["latitude"]
+        user_lon = user["longitude"]
+
+        user_coords = (user_lat, user_lon)
+
+        distance_from_london = distance.distance(LONDON_COORDS, user_coords).miles
+
+        if distance_from_london <= radius:
+            # add user to valid list
+            pass
+    
+    return valid_users
