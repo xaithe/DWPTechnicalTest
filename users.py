b9630a7164363801b58f2da4d114769cf3d83a55
Tom Gulliver <tgulliver97@gmail.com>
2020-08-15 00:24:53 +0100

    defined function to get users in a given city

--- users.py
+++ users.py
@@ -2,9 +2,17 @@ import requests
 
 API_URL = "https://bpdts-test-app.herokuapp.com/"
 USERS_URL = API_URL + "users"
-CITY_URL = API_URL + "city"
+CITY_URL = API_URL + "city/"
 
-def get_users(url):
+def get_users():
     req = requests.get(USERS_URL)
 
     return req.json()
+
+def get_city_users(city):
+    uri = CITY_URL + city + "/users"
+
+    req = requests.get(uri)
+
+    return req.json()
+
