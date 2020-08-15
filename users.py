aea284213ae3e5360a10b6c4bd304de8b8b674d9
Tom Gulliver <tgulliver97@gmail.com>
2020-08-15 12:54:12 +0100

    Added functionality for getting a specific user

--- users.py
+++ users.py
@@ -2,10 +2,18 @@ import requests
 
 API_URL = "https://bpdts-test-app.herokuapp.com/"
 USERS_URL = API_URL + "users"
+USER_URL = API_URL + "user/"
 CITY_URL = API_URL + "city/"
 
 def get_users():
     req = requests.get(USERS_URL)
+
+    return req.json()
+
+def get_user(id):
+    uri = USER_URL + str(id)
+
+    req = requests.get(uri)
 
     return req.json()
 
