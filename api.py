33017543b5edb13320813a575a2e650561b8befa
Tom Gulliver <tgulliver97@gmail.com>
2020-08-15 21:42:00 +0100

    Error responses are now valid JSON

--- api.py
+++ api.py
@@ -4,6 +4,7 @@ import geocoding
 
 app = Flask(__name__)
 
+# Routes
 @app.route('/',methods=['GET'])
 def index():
     return "<h1>DWP Tech Test API</h1><p>This is an API for the DWP technical test</p>"
@@ -20,11 +21,17 @@ def get_users_in_radius():
         try:
             miles = float(request.args["miles"])
         except ValueError:
-            return "Error: miles must be a valid float", 400
+            return jsonify(error="miles must be a valid float"), 400
     else:
         miles = 50
 
     return jsonify(geocoding.users_in_radius(user_list, miles)), 200
+
+# 404 JSON response
+@app.errorhandler(404)
+def invalid_route(e):
+    return jsonify(error="404 Not Found"), 404
+
 
 if __name__ == '__main__':
     app.run(debug=True)
