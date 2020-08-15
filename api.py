423aeaa1d88d118723279c94c9c39007aa1b8981
Tom Gulliver <tgulliver97@gmail.com>
2020-08-15 12:55:35 +0100

    added /london and /radius endpoints

--- api.py
+++ api.py
@@ -1,10 +1,22 @@
-from flask import Flask
+from flask import Flask, jsonify
+import users
+import geocoding
 
 app = Flask(__name__)
 
 @app.route('/',methods=['GET'])
 def index():
     return "Hello World!"
+
+@app.route('/london', methods=['GET'])
+def get_users_in_london():
+    return jsonify(users.get_city_users("London"))
+
+@app.route('/radius', methods=['GET'])
+def get_users_in_radius():
+    user_list = users.get_users()
+   
+    return jsonify(geocoding.users_in_radius(user_list, 100))
 
 if __name__ == '__main__':
     app.run(debug=True)
